d33d4aa88447c3166a199a7f2c5cef90733d9401
Zakariae <Zakariaeabassioff@gmail.com>
2023-12-30 06:18:29 +0100

    Build of the avatar & indicator component

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,19 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  //Fichier à inclure dans le build
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    //Ajout de couleurs personnalisées
     extend: {
+      borderWidth: {
+        avatar: '6px',
+      },
       colors: {
         supernova: '#fac900' /* yellow / warning */,
-        spicyMustard: '#6e5a0d' /* dark yellow */,
-        bombay: '#aeaeae' /* light gray */,
         orangeRed: '#ff4600' /* red / error */,
-        cognac: '#9a360e' /* dark red */,
-        fireEngineRed: '#c52425' /* dark red */,
         silverTree: '#70bd91' /* light green */,
-        turquoiseGreen: '#a0ccb7' /* very light green */,
         spanishGreen: '#008a52' /* medium green */,
         cruseo: '#0a5d2c' /* dark green */,
         countyGreen: '#003d19' /* darkest green */,
@@ -22,11 +18,15 @@ module.exports = {
         nileBlue: '#243853' /* blue */,
         blackPearl: '#071126' /* navy blue */,
         pastelMagenta: '#ff9dbf' /* pink */,
-        padua: '#b1e3cc' /* lightest green - placeholder color */,
-        vistaBlue: '#94d1b4' /* light green - placeholder color */,
+        vistaBlue: '#94d1b4' /* light green around the avatar */,
+      },
+      height: {
+        18: '72px',
+      },
+      width: {
+        18: '72px',
       },
     },
-    //Override des polices par défaut
     fontFamily: {
       condensed: ['Bebas Neue', 'sans-serif'],
       handwriting: ['Kalam', 'cursive'],
---
+++ web/src/components/Indicator/Indicator.stories.tsx
@@ -0,0 +1,44 @@
+// Pass props to your component by passing an `args` object to your story
+//
+// ```tsx
+// export const Primary: Story = {
+//  args: {
+//    propName: propValue
+//  }
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Indicator from './Indicator'
+
+const meta: Meta<typeof Indicator> = {
+  component: Indicator,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof Indicator>
+
+export const Succes: Story = {
+  args: {
+    status: 'success',
+  }
+}
+
+export const Error: Story = {
+  args: {
+    status: 'error',
+  }
+}
+
+
+export const Warning: Story = {
+  args: {
+    status: 'warning',
+  }
+}
+
