b6973917ae3269c2566994b8f09a12dd04bdd156
Zakariae <Zakariaeabassioff@gmail.com>
2023-12-31 03:45:30 +0100

    Implemented Show/hide option in password field.

---
+++ web/src/components/ShowHidePassword/ShowHidePassword.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import Icon from '../Icon/Icon'
+import { Label, PasswordField, TextField } from '@redwoodjs/forms'
+
+const ShowHidePassword = ({ label, name, ...rest }) => {
+  const [isPasswordShowing, setisPasswordShowing] = useState(false)
+
+  const handleClick = () => {
+    setisPasswordShowing((prevValue) => !prevValue)
+  }
+
+  return (
+    <div className="field relative">
+      <Label name={name}>{label}</Label>
+      {isPasswordShowing ? (
+        <TextField name={name} placeholder="" {...rest} />
+      ) : (
+        <PasswordField
+          name={name}
+          placeholder=""
+          {...rest}
+        />
+      )}
+      <button className="absolute right-6 top-8" onClick={handleClick}>
+        {isPasswordShowing ? (
+          <Icon id="eyeOpened" size={32} />
+        ) : (
+          <Icon id="eyeClosed" size={32} />
+        )}
+      </button>
+    </div>
+  )
+}
+
+export default ShowHidePassword
